188db7b6ada7c07d5a51082dadcfebb5bcec5c91
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-01-07 21:05:14 +0530

    Create 7_1_2022.cpp

---
+++ Jan 2022/7_1_2022.cpp
@@ -0,0 +1,126 @@
+// 382. Linked List Random Node
+// Medium
+
+// 1476
+
+// 372
+
+// Add to List
+
+// Share
+// Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+// Implement the Solution class:
+
+// Solution(ListNode head) Initializes the object with the integer array nums.
+// int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be choosen.
+ 
+
+// Example 1:
+
+
+// Input
+// ["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+// [[[1, 2, 3]], [], [], [], [], []]
+// Output
+// [null, 1, 3, 2, 2, 3]
+
+// Explanation
+// Solution solution = new Solution([1, 2, 3]);
+// solution.getRandom(); // return 1
+// solution.getRandom(); // return 3
+// solution.getRandom(); // return 2
+// solution.getRandom(); // return 2
+// solution.getRandom(); // return 3
+// // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ 
+
+// Constraints:
+
+// The number of nodes in the linked list will be in the range [1, 104].
+// -104 <= Node.val <= 104
+// At most 104 calls will be made to getRandom.
+ 
+
+// Follow up:
+
+// What if the linked list is extremely large and its length is unknown to you?
+// Could you solve this efficiently without using extra space?
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <experimental/random> // for randint function
+class Solution {
+public:    
+    
+//     FIND OUT WHY THIS IS WRONG 
+    
+//     int max_no = INT_MIN, min_no = INT_MAX;
+    
+//     Solution(ListNode* head) {
+            
+//         ListNode* temp = head;
+
+//         while(temp != NULL)
+//         {
+//             if (temp->val > max_no)
+//                 max_no = temp->val;
+            
+//             if (temp->val < min_no)
+//                 min_no = temp->val;
+            
+//             temp = temp->next;
+//         }
+        
+                
+//         cout << min_no << " " << max_no << endl;
+ 
+//     }
+    
+//     int getRandom() {
+
+//         return experimental::randint(min_no, max_no);
+
+//     }
+    
+    ListNode* temp1 = NULL;
+    
+    Solution(ListNode * head)
+    {
+        temp1 = head;
+    }
+    
+    int getRandom() {
+        
+        int index = 1, ans = 0;
+        
+        ListNode* temp = temp1;
+        
+        while(temp != NULL)
+        {
+            if (rand() % index == 0)
+                ans = temp->val;
+            
+            index++;
+            temp = temp->next;
+        }
+        
+        return ans;
+    }
+    
+    
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
