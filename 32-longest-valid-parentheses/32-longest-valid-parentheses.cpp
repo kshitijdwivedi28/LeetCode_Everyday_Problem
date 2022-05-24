4d44a2d846defe54d8d1cd843e4041a683e3b8d1
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-05-24 13:58:52 +0530

    Time: 3 ms (88.93%), Space: 7.3 MB (42.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -2,67 +2,70 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         
-//         stack<char> temp_stack;
-//         deque<char> dq;
-//         int max_length = 0, ans = INT_MIN;
+        if (s.empty()) return 0;
         
-//         for( int i = 0; i < s.size(); i++)
-//         {
-//             if (s[i] == '(')
-//                 dq.push_front(s[i]);
-                
-//             else 
-//             {
-//                 if (dq.front() == '(')
-//                 {
-//                     max_length++;
-//                     dq.pop_front();
-//                 }
-//             }
-            
-//             ans = max(ans, max_length);
-//         }
+        // using stack to store index instead of parentheses like in valid parentheses
+        // bcoz length is required.
+        // So, will subtract starting index of last parentheses with the current parentheses
+        // to get the longest length
         
-//         return ans*2;
+        stack<int> st;
+        int ans = INT_MIN;
+        
+        // to handle the case of '()'
+        st.push(-1);
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(') st.push(i);
+            else
+            {
+                st.pop();
+                if (st.empty()) st.push(i);
+                else ans = max(ans, (i - st.top()));
+            }
+        }
+        
+        return ans == INT_MIN ? 0 : ans;
         
         // using two pointer approach
         // 2 time traversals
         // left to right and right to left
         
-        int l,r,ans;
-        l = r = ans = 0;
+//         int l, r, ans;
+//         l = r = ans = 0;
         
-        for(auto value : s)
-        {
-            if(value == '(')
-                l++;
-            else
-                r++;
+//         for(auto value : s)
+//         {
+//             if(value == '(')
+//                 l++;
+//             else
+//                 r++;
             
-            if (l == r)
-                ans = max(ans, 2*l);
+//             if (l == r)
+//                 ans = max(ans, 2*l);
                 
-            else if (r > l)
-                l = r = 0;
-        }
+//             else if (r > l)
+//                 l = r = 0;
+//         }
         
-        l = r = 0;
+//         l = r = 0;
         
-        for(int i = s.size()-1; i >= 0; i--)
-        {
-            if(s[i] == '(')
-                l++;
-            else
-                r++;
+//         for(int i = s.size()-1; i >= 0; i--)
+//         {
+//             if(s[i] == '(')
+//                 l++;
+//             else
+//                 r++;
             
-            if (l == r)
-                ans = max(ans, 2*l);
+//             if (l == r)
+//                 ans = max(ans, 2*l);
                 
-            else if (l > r)
-                l = r = 0;
-        }
+//             else if (l > r)
+//                 l = r = 0;
+//         }
         
-        return ans;
+//         return ans;
         
     }
 };
