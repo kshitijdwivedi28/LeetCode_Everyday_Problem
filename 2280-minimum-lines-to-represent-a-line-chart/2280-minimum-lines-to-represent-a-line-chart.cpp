eea92da4ea8e386b4f3f3c68f079dd373720dadc
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-05-25 18:13:51 +0530

    Time: 638 ms (64.89%), Space: 114.6 MB (10.76%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
-    
-//     bool slope(vector<vector<int>>& sp, int x, int y, int z)
-//     {
-//         return (float)(sp[y][1] - sp[x][1]) == ((double)(sp[z][1] - sp[x][1]) / (double)(sp[z][0] - sp[x][0]))*(float)(sp[y][0] - sp[x][0]);
-//     }
-//     int minimumLines(vector<vector<int>>& sp) {
-        
-//         if (sp.size() == 1)
-//             return 0;
-        
-//         sort(sp.begin(), sp.end());
 
-//         int ans = 1;
-        
-//         for(int i = 2; i < sp.size(); i++)
-//             if (slope(sp, i-2, i-1, i) == false)
-            
-//                 ans++;
-        
-//         return ans;
-//     }
-    
-    
     int minimumLines(vector<vector<int>>& sp) 
     {
         if (sp.size() == 1)
