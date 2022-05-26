0e68ddd6c96d1ef8e4b3b366873af031aa3bd27d
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-05-26 15:11:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,7 +2,17 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
+        
+        int ans = 0;
+        
+        for(uint32_t i = 0; i <= 31; i++)
+            if (n & (1 << i))
+                ans++;
+        
+        return ans;
+            
+        
         
     }
 };
