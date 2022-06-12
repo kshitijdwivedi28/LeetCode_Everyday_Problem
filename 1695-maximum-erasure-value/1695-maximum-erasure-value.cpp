c895159a0b3c78907468ffbaf03816bb9b2b1ded
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-06-12 17:22:00 +0530

    Time: 257 ms (91.86%), Space: 92.3 MB (93.32%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -2,7 +2,33 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         
-        unordered_set<int> st;
+//         unordered_set<int> st;
+
+//         // sliding window or two pointers
+        
+//         int l = 0, r = 0, n = nums.size(), max_sum = INT_MIN, ans = 0;
+        
+//         while(l < n && r < n)
+//         {
+//             if (st.find(nums[r]) == st.end())
+//             {
+//                 st.insert(nums[r]);
+//                 ans += nums[r];
+//                 max_sum = max(ans, max_sum);
+//                 r++;
+//             }
+//             else
+//             {
+//                 st.erase(nums[l]);
+//                 ans -= nums[l];
+//                 l++;
+//             }
+//         }
+        
+//         return max_sum;
+        
+        
+        vector<int> occur(10001, 0);
 
         // sliding window or two pointers
         
@@ -10,16 +36,16 @@ public:
         
         while(l < n && r < n)
         {
-            if (st.find(nums[r]) == st.end())
+            if (occur[nums[r]] == 0)
             {
-                st.insert(nums[r]);
+                occur[nums[r]]++;
                 ans += nums[r];
                 max_sum = max(ans, max_sum);
                 r++;
             }
             else
             {
-                st.erase(nums[l]);
+                occur[nums[l]]--;
                 ans -= nums[l];
                 l++;
             }
