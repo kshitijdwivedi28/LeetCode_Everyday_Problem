d941c1f4454d9e9db20894fe6b99d2dd28556589
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-06-22 13:19:53 +0530

    Time: 4 ms (44.07%), Space: 5.9 MB (80.88%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     int minimumNumbers(int num, int k) {
         
+//         if (num == 0) return 0;
+//         if (k > num) return -1;
+//         if (k == num || num%10 == k) return 1;
+//         if (k == 0) return -1;
+        
+//         int ans = 0;
+//         bool check = false;
+        
+//         for(int i = num; i >= 0; i-=k)
+//         {
+//             ans++;
+//             if (i%10 == k)
+//             {
+//                 check = true;
+//                 break;
+//             }
+//         }
+        
+//         return check == false ? -1 : ans;
+            
         if (num == 0) return 0;
-        if (k > num) return -1;
-        if (k == num || num%10 == k) return 1;
-        if (k == 0) return -1;
         
-        int ans = 0;
-        bool check = false;
+        for(int i = 1; i <= 10; i++)
+            if (i*k <= num && num%10 == (i*k)%10) return i;
         
-        for(int i = num; i >= 0; i-=k)
-        {
-            ans++;
-            if (i%10 == k)
-            {
-                check = true;
-                break;
-            }
-        }
-        
-        return check == false ? -1 : ans;
+        return -1;
     }
 };
