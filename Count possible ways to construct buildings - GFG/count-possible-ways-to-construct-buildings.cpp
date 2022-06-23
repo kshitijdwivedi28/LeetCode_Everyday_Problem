222a56cfe665e250b05a5975e7cbb7f4c88f862a
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-06-23 13:13:18 +0530

    Added solution - LeetHub

---
+++ Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+	int TotalWays(int N)
+	{
+	    // Code here
+        int mod = 1e9 + 7;
+	    
+	    vector<long long> fib(N+3, 0);
+	    fib[0] = 0, fib[1] = 1;
+	    
+	    for(int i = 2; i < N+3; i++)
+	        fib[i] = fib[i-1]%mod + fib[i-2]%mod;
+
+	    return (fib[N+2]%mod * fib[N+2]%mod)%mod;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int N;
+		cin >> N;
+		Solution ob;
+		int ans = ob.TotalWays(N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
