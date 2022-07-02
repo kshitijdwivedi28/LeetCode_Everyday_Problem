c1904fbbb45dc1de1ba3e12cde2c57962bb5518a
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-07-02 15:24:03 +0530

    Time: 88 ms (76.56%), Space: 32.3 MB (76.96%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) 
+    {
+        // APPROACH - 
+        // Calculate maximum difference between two cuts in horizontal array and vertical array
+        // and then multiply them to get the maximum area.
+        
+        // sorting to get all the cuts in proper format ordered.
+        sort(horizontalCuts.begin(), horizontalCuts.end());
+        sort(verticalCuts.begin(), verticalCuts.end());
+        
+        // finding max length among initial and final pieces by horizontal cuts
+        int max_horizontal_cut = max(horizontalCuts[0], h - horizontalCuts.back());
+        
+        // finding max breath among initial and final pieces by vertical cuts
+        int max_vertical_cut = max(verticalCuts[0], w - verticalCuts.back());
+        
+        // Calculating max length among all horizontal cuts
+        for(int i = 1; i < horizontalCuts.size(); i++)
+            max_horizontal_cut = max(max_horizontal_cut, horizontalCuts[i] - horizontalCuts[i-1]);
+        
+        // Calculating max breadth among all vertical cuts
+        for(int i = 1; i < verticalCuts.size(); i++)
+            max_vertical_cut = max(max_vertical_cut, verticalCuts[i] - verticalCuts[i-1]);
+
+        // Calculating and returning max area mod 1e9+7
+        return (int)((long)max_horizontal_cut * max_vertical_cut % 1000000007);
+
+    }
+};
