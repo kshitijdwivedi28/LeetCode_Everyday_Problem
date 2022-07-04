55b1f39a3ee978cb21813544ba46fb95ee0cb116
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-07-04 11:36:42 +0530

    Added solution - LeetHub

--- Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
+++ Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
@@ -14,25 +14,58 @@ class Solution {
         // if (__builtin_popcount(A) == __builtin_popcount(B))
         //     return A;
         
-        int setbits = __builtin_popcount(b);
-		int ans = 0;
-		for (int i = 30; i >= 0; i--) {
-			if ((a >> i) & 1) {
-				if (setbits > 0) {
-					setbits--;
-					ans += (1 << i);
-				}
-			}
-		}
-		for (int i = 0; i <= 30; i++) {
-			if (!((ans >> i) & 1)) {
-				if (setbits > 0) {
-					setbits--;
-					ans += (1 << i);
-				}
-			}
-		}
-		return ans;
+        
+        int b_set_bits = __builtin_popcount(b);
+        
+        if (__builtin_popcount(a) == b_set_bits) return a;
+        
+        int ans = 0;
+        
+        for(int i = 31; i >= 0; i--)
+        {
+            if (a & (1 << i))
+            {
+                if (b_set_bits == 0) return ans;
+                
+                b_set_bits--;
+                ans |= (1 << i);
+            }
+        }
+        
+        for(int i = 0; i < 32; i++)
+        {
+            if (!(a & (1 << i))) 
+            {
+                if (b_set_bits == 0) break;
+                
+                 b_set_bits--;
+                ans |= (1 << i);
+            }
+           
+        }
+        
+        return ans;
+        
+        
+//         int setbits = __builtin_popcount(b);
+// 		int ans = 0;
+// 		for (int i = 30; i >= 0; i--) {
+// 			if ((a >> i) & 1) {
+// 				if (setbits > 0) {
+// 					setbits--;
+// 					ans += (1 << i);
+// 				}
+// 			}
+// 		}
+// 		for (int i = 0; i <= 30; i++) {
+// 			if (!((ans >> i) & 1)) {
+// 				if (setbits > 0) {
+// 					setbits--;
+// 					ans += (1 << i);
+// 				}
+// 			}
+// 		}
+// 		return ans;
             
         
         
